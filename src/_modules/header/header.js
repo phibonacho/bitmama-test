51ed6889ed4f5a911880f112111768f150ca3fb5
Cristian Gardosi <cristian.gardosi97@gmail.com>
2021-09-03 17:43:34 +0200

    Code reafctor before weekend!

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -8,15 +8,4 @@ export default class Header {
 }
 
 
-/*document.addEventListener('DOMContentLoaded', function () {
-  var dropdownItem = document.querySelector('.dropdown-item');
-  var dropdown = document.querySelector('.dropdown');
 
-  dropdownItem.addEventListener('mouseenter', function (e) {
-      dropdown.classList.add('dropdown-show');
-  });
-  dropdownItem.addEventListener('mouseleave', function () {
-      dropdown.classList.remove('dropdown-show');
-  })
-
-})*/
