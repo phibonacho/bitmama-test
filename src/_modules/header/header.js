25e8fbc024c5fdb12e9db4982443d5ed798bd728
Cristian Gardosi <cristian.gardosi97@gmail.com>
2021-09-03 13:03:50 +0200

    Styling upgrade e JS study

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -6,3 +6,17 @@ export default class Header {
     console.log('%s module', this.name.toLowerCase());
   }
 }
+
+
+/*document.addEventListener('DOMContentLoaded', function () {
+  var dropdownItem = document.querySelector('.dropdown-item');
+  var dropdown = document.querySelector('.dropdown');
+
+  dropdownItem.addEventListener('mouseenter', function (e) {
+      dropdown.classList.add('dropdown-show');
+  });
+  dropdownItem.addEventListener('mouseleave', function () {
+      dropdown.classList.remove('dropdown-show');
+  })
+
+})*/
