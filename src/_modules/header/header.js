adc0660c694c913e0cd9ee0450ffee4fc4a316a4
Andrea Storace <andrea.storace.uni@gmail.com>
2021-09-07 14:20:36 +0200

    revised menu animation

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -6,7 +6,20 @@ export default class Header {
     console.log('%s module', this.name.toLowerCase());
 
     document.getElementById('mobile-menu-button').addEventListener('click', (event) => {
-      document.getElementById('mobile-menu').classList.toggle('active');
+      let mobileMenu = document.getElementById('mobile-menu');
+
+      if(mobileMenu.style.display === 'none') {
+        mobileMenu.style.display = 'block';
+        setTimeout(() => {
+          mobileMenu.classList.toggle('active');
+        }, 200);
+      } else {
+        mobileMenu.classList.toggle('active');
+        setTimeout(() => {
+          mobileMenu.style.display = 'none';
+        }, 500);
+      }
+
       document.documentElement.classList.toggle('stop-scroll');
     });
   }
