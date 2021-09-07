c1aff53663a7896410a9f0d475cf5d3e9dee6453
Andrea Storace <andrea.storace.uni@gmail.com>
2021-09-07 10:58:19 +0200

    foxed scroll issue

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -4,6 +4,11 @@ export default class Header {
   constructor() {
     this.name = 'Header';
     console.log('%s module', this.name.toLowerCase());
+
+    document.getElementById('mobile-menu-button').addEventListener('click', (event) => {
+      document.getElementById('mobile-menu').classList.toggle('active');
+      document.documentElement.classList.toggle('stop-scroll');
+    });
   }
 }
 
