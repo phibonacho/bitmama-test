a69e11455c1a0e128dbf9bb2686a048971791c82
Cristian Gardosi <cristian.gardosi97@gmail.com>
2021-09-07 15:52:55 +0200

    Mobile menu languages options

--- src/_modules/mobilemenu/mobileMenu.js
+++ src/_modules/mobilemenu/mobileMenu.js
@@ -4,5 +4,23 @@ export default class MobileMenu {
   constructor() {
     this.name = 'mobileMenu';
     console.log('%s module', this.name.toLowerCase());
+
+    let content = document.getElementById('dropdown-content');
+    content.addEventListener('click', (event) => event.stopPropagation());
+
+    document.getElementById('dropdown-toggle').addEventListener('click', (event) => {
+
+      if(content.style.display === 'none') {
+        content.style.display = 'flex';
+        setTimeout(() => {
+          content.classList.toggle('dropdown__options--active');
+        }, 200);
+      } else {
+        content.classList.toggle('dropdown__options--active');
+        setTimeout(() => {
+          content.style.display = 'none';
+        }, 500);
+      }
+    })
   }
 }
